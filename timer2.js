88dfd3bbe13dcef81a66bf04b1225b29caebaa29
Keyvan <k.kholghi@gmail.com>
2019-09-25 00:20:06 +0000

    Revised timer2.js by listening for user input and set timers.

--- timer2.js
+++ timer2.js
@@ -9,10 +9,15 @@ stdin.setEncoding('utf8');
 
 // on any input from stdin (standard input), output a "." to stdout
 stdin.on('data', (key) => {
-    if (key === '\u0003') {
+    if (key === 'b') {
+        process.stdout.write(`\x07`);
+    } else if (key >= 1 && key <= 9) {
+        console.log(`setting timer for ${key} seconds...`);
+        setTimeout(() => {
+            process.stdout.write(`\x07`);
+        }, key * 1000)
+    } else if (key === '\u0003') {
         process.exit();
     }
-    process.stdout.write('.');
 });
 
-console.log('after callback');
